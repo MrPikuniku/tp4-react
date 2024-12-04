74b08f6a8c1c1478719cd68932273ab1a8d72ee8
MrPikuniku <75395240+MrPikuniku@users.noreply.github.com>
2024-12-04 09:54:41 +0100

    question 1.1 : Création page de dev
    Page de Cart

---
+++ src/components/product-grid.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@arthur.eudeline/starbucks-tp-kit/components/button";
+import { ProductCardLayout } from "@arthur.eudeline/starbucks-tp-kit/components/products/product-card-layout";
+import { ProductGridLayout } from "@arthur.eudeline/starbucks-tp-kit/components/products/product-grid-layout";
+import { ProductsCategoryData } from "@arthur.eudeline/starbucks-tp-kit/types";
+import Link from "next/link";
+import { type FC } from "react";
+
+type Props = {
+  categories: ProductsCategoryData[]
+}
+
+export const ProductGrid: FC<Props> = function ({ categories }) {
+  return <div className="space-y-24 relative">
+    {categories.map((cat) => (
+      <section key={cat.id}>
+        {/* Titre de la catégorie (ex: Latte (11)) */}
+        <Link href={`/${cat.slug}`} className="link">
+          <h2 className="text-lg font-semibold mb-8 tracking-tight">
+            {cat.name} ({cat.products.length})
+          </h2>
+        </Link>
+
+        {/* Grille des produits pour la catégorie courante */}
+        <ProductGridLayout products={cat.products}>
+          {(product) => (
+            // Carte produit
+            <ProductCardLayout
+              product={product}
+              button={<Button product={product} variant={"ghost"} fullWidth>
+                Ajouter au panier
+              </Button>}
+            />
+          )}
+        </ProductGridLayout>
+      </section>
+    ))}
+  </div>;
+};
